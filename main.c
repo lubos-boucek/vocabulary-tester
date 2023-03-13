ee613521a6c2ae15bcdf36d9750c699b731b278f
Lubos Boucek <boucek.lubos@protonmail.com>
2023-03-13 01:30:59 +0100

    Distinguish the parsing state of the actual line

--- main.c
+++ main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <err.h>
+#include <ctype.h>
 
-/* TODO: Configurable constants */
-const char *file_name = "learners.en.txt";
+#include "main.h"
+
+int process_line(const char *, ssize_t, enum parsing_state *);
 
 int
 main(int argc, char **argv)
@@ -12,11 +14,12 @@ main(int argc, char **argv)
 	char *line = NULL;
 	size_t line_size = 0;
 	ssize_t line_length;
+	enum parsing_state state = BETWEEN;
 
 	fp = fopen(file_name, "r");
 	
 	while ((line_length = getline(&line, &line_size, fp)) != -1) {
-		fwrite(line, line_length, 1, stdout);
+		process_line(line, line_length, &state);
 	}
 
 	free(line);
@@ -25,5 +28,31 @@ main(int argc, char **argv)
 
 	fclose(fp);
 
+	return 0;
+}
+
+int
+process_line(const char *line, ssize_t line_length, enum parsing_state *state)
+{
+	int is_blank = 1;
+	ssize_t i = 0;
+	
+	while (i < line_length - 1) {
+		if (!isspace(line[i--])) {
+			is_blank = 0;
+			break;
+		}
+	}	
+
+ 	fwrite(line, line_length, 1, stdout);
+
+	if (*state == BETWEEN && !is_blank) {
+		*state = HEADWORD;
+	} else if (!is_blank) {
+		*state = ENTRY;
+	} else {
+		*state = BETWEEN;
+	}
+	
 	return 0;
 }
---
+++ main.h
@@ -0,0 +1,4 @@
+/* TODO: Configurable constants */
+const char *file_name = "learners.en.txt";
+
+enum parsing_state {BETWEEN, HEADWORD, ENTRY};
